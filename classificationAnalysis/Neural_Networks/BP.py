b6d8c677b9de2410dbad8fa512124de124c6ca73
Limlin-Qs <limlin95@126.com>
2021-09-10 20:39:05 +0800

    tips: added new algorithm BP network

---
+++ classificationAnalysis/Neural_Networks/BP.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+
+"""
+  Author: limlin
+  Contact: limlin95@126.com
+  Datetime: 2021/9/8 22:45
+  Software: PyCharm
+  Profile:
+"""
---
+++ classificationAnalysis/Neural_Networks/BP0.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8 -*-
+
+"""
+  Author: limlin
+  Contact: limlin95@126.com
+  Datetime: 2021/9/10 20:35
+  Software: PyCharm
+  Profile:
+"""
